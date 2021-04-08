01df434777f8ff02f381cbbf7b6d979522f7d3fe
ROHITH-Singh <rs9113848@gmail.com>
2021-04-08 13:19:14 +0530

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+const express = require('express');
+const app = express();
+// const cors = require('cors')
+const port = process.env.PORT || 9000
+
 const { Client } = require('pg');
 
+
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || 'postgres://ccaoexshbkbatc:f589d4327b348970d99039b08cdf668b40e1a77a8c262a2583fbfa65fd6f280a@ec2-52-21-153-207.compute-1.amazonaws.com:5432/dfl00icf43gult ',
   ssl: {
     rejectUnauthorized: false
   }
 });
 
 client.connect();
+client.query('SELECT * FROM user ',(err,res)=>{
+    console.log(err,res);
+    client.end()
+})
+
+app.listen(port,function(){
+    console.log('server started')
+  })
