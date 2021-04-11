f2ed0fedda1950408d24cdff44ef55736265f9b8
ROHITH-Singh <rs9113848@gmail.com>
2021-04-12 00:28:12 +0530

    make it better

--- routers/user/login.js
+++ routers/user/login.js
@@ -6,7 +6,7 @@ var bcrypt = require('bcryptjs');
 var d = new Date();
 const { Client } = require('pg');
 
-router1.use(express.json());
+router2.use(express.json());
 const client = new Client({
   connectionString: process.env.DATABASE_URL || 'postgres://ccaoexshbkbatc:f589d4327b348970d99039b08cdf668b40e1a77a8c262a2583fbfa65fd6f280a@ec2-52-21-153-207.compute-1.amazonaws.com:5432/dfl00icf43gult ',
   ssl: {
