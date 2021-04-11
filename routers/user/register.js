4d4f7c23c3a2cf6e731f818b30f85754adbf0e3c
ROHITH-Singh <rs9113848@gmail.com>
2021-04-11 22:47:50 +0530

    make it better

--- routers/user/register.js
+++ routers/user/register.js
@@ -56,7 +56,7 @@ router1.post('/',async(req,res)=>{
                     else{
                       client.query(`INSERT INTO user-details(name,email,password,date)
                        VALUES($1,$2,$3,$4)
-                       RETURNING user_id,password `[name,email,hashedPassword,d],(err,results)=>{
+                       RETURNING user_id,password `,[name,email,hashedPassword,d],(err,results)=>{
                          if (err)
                          throw err;
                          else{
