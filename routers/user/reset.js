a6ee7e944beea8aa246d7089d13a572e0c833859
ROHITH-Singh <rs9113848@gmail.com>
2021-04-12 01:49:34 +0530

    make it better

--- routers/user/reset.js
+++ routers/user/reset.js
@@ -19,7 +19,7 @@ client.connect();
 router3.post('/',async(req,res) => {
    let {email,password} = req.body;
 
-   client.query(`UPDATE user_details SET password=${password} WHERE email=${email} RETURNING *`,[password,email],(err,result)=>{
+   client.query(`UPDATE user_details SET password=$1 WHERE email=$2 RETURNING *`,[password,email],(err,result)=>{
        if(err){
            res.send({message:false,type:err})
        }
